46d966b7715000fcd2e3889a4f3a1c60c4008ea0
Mahima <mahimamishra5008@gmail.com>
2024-11-22 12:33:07 +0530

    made first basic project of bgcolor change

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,43 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState('olive');
-
-  function changeColor(color) {
-    setColor(color);
-  }
-
+  // function changeColor(color){
+  //   setColor(color);
+  // }
   return (
     <div 
-      className='w-full h-screen transition-colors duration-200' 
-      style={{ backgroundColor: color }}
-    > 
+      className='w-full h-screen  duration-200' 
+      style={{ backgroundColor: color}}> 
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
           <button 
-            onClick={() => changeColor('red')}
+            onClick={() => setColor('red')} style={{backgroundColor: "red"}}
             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
-          >
-            Red
+          >            Red
           </button>
           <button 
-            onClick={() => changeColor('green')}
+            onClick={() => setColor('green')} style={{backgroundColor: "green"}}
             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
           >
             Green
           </button>
+          <button 
+            onClick={() => setColor('violet')} style={{backgroundColor: "violet"}}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+          >            Violet
+          </button>
+          <button 
+            onClick={() => setColor('blue')} style={{backgroundColor: "blue"}}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+          >            Blue
+          </button>
+          <button 
+            onClick={() => setColor('orange')} style={{backgroundColor: "orange"}}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+          >            Orange
+          </button>
         </div>
+        
       </div>
     </div>
   );
